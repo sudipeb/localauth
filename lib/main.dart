f4c8a1086640901f96fa25e3772e32a13da8edcf
sudipeb <sudip.koirala@ebpearls.com>
2026-02-09 11:52:34 +0545

    :up: update: logic for local auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:localauth/auth_gate.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(context) {
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Local Auth Implementaiton", home: AuthGate());
   }
 }
